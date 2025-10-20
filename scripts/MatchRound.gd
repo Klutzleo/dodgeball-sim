7eb0febf1ff269a756b69f14655182a4ec70a779
Klutzleo <klutzleo@gmail.com>
2025-10-20 16:44:22 -0700

    Update MatchRound.gd
    Added stat breakdown in MatchRound

--- scripts/MatchRound.gd
+++ scripts/MatchRound.gd
@@ -1,9 +1,16 @@
 class_name MatchRound
+extends Resource
 
 var turn: int
 var thrower: Player
 var target: Player
 var outcome: String
 var commentary: String
-var revived_player: Player
-var ball_holder_after: Player
+var revived_player: Player = null
+var ball_holder_after: Player = null
+
+# 🪓 New: Stat breakdown
+var throw_power: int
+var dodge_power: int
+var catch_power: int
+var roll: int
